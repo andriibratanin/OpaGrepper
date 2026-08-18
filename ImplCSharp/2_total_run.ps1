c28c13e8c461eb9ae79cee059ae9eca061177de4
Andrii Bratanin <zmmx-aerial@ukr.net>
2026-08-18 05:13:37 +0300

    ImplCSharp: add run scripts

--- ImplCSharp/2_total_run.ps1
+++ ImplCSharp/2_total_run.ps1
@@ -1,3 +1,22 @@
 # Script to run the C# file based application with source in `2_total.cs` file
 
+# Data source URL
+# Executive proceedings
+$env:SOURCE_URL = "https://data.gov.ua/dataset/22aef563-3e87-4ed9-92e8-d764dc02f426/resource/d1a38c08-0f3a-4687-866f-f28f50df7c46/download/28-ex_csv_asvp.zip"
+# Debtors
+#$env:SOURCE_URL = "https://data.gov.ua/dataset/783b9b50-faba-4cc9-a393-60485e395b1d/resource/e6ea76c1-01f4-4bd0-a282-7d92d6ecc2a1/download/29-ex_csv_erb.zip"
+
+# Flag signaling that Wind1251 -> UTF-8 re-encoding is needed
+# Note: must be exactly "true" or "false" string (case insensitive, default is "true")
+#$env:ICONV = "true"
+#$env:ICONV = "false"
+
+# Where to place results
+# Note: this is not a "Data" (possibly pre-cached data source) directory location
+$env:RESULT_DIR = (Resolve-Path (Join-Path $PSScriptRoot "..\Data")).Path
+
 dotnet run 2_total.cs
+
+#$env:SOURCE_URL = $null
+#$env:ICONV = $null
+#$env:RESULT_DIR = $null
--- ImplCSharp/2_total_run.sh
+++ ImplCSharp/2_total_run.sh
@@ -7,4 +7,24 @@
 # - sudo apt install dotnet-sdk-10.0
 #
 
+# Data source URL
+# Executive proceedings
+export SOURCE_URL="https://data.gov.ua/dataset/22aef563-3e87-4ed9-92e8-d764dc02f426/resource/d1a38c08-0f3a-4687-866f-f28f50df7c46/download/28-ex_csv_asvp.zip"
+# Debtors
+#export SOURCE_URL="https://data.gov.ua/dataset/783b9b50-faba-4cc9-a393-60485e395b1d/resource/e6ea76c1-01f4-4bd0-a282-7d92d6ecc2a1/download/29-ex_csv_erb.zip"
+
+# Flag signaling that Win1251 -> UTF-8 re-encoding is needed
+# Note: must be exactly "true" or "false" string (case insensitive, default is "true")
+#export ICONV="true"
+#export ICONV="false"
+
+# Where to place results
+# Note: this is not a "Data" (possibly pre-cached data source) directory location
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+export RESULT_DIR="$(realpath "$SCRIPT_DIR/../Data")"
+
 dotnet run 2_total.cs
+
+#unset SOURCE_URL
+#unset ICONV
+#unset RESULT_DIR
