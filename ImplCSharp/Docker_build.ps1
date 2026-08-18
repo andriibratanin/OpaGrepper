161b1c17a0218ff738d56f470416ef0188364ea3
Andrii Bratanin <zmmx-aerial@ukr.net>
2026-08-18 05:14:55 +0300

    ImplCSharp: add Docker build scripts

---
+++ ImplCSharp/Docker_build.ps1
@@ -0,0 +1,5 @@
+# Script to build a Docker image with main executable
+
+docker build -f Dockerfile . -t opa-grepper
+# OR
+#docker build -f Dockerfile.Ubuntu . -t opa-grepper
---
+++ ImplCSharp/Docker_build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Script to build a Docker image with main executable
+
+docker build -f Dockerfile . -t opa-grepper
+# OR
+#docker build -f Dockerfile.Ubuntu . -t opa-grepper
