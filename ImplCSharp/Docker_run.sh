0469f5c8122d5bffa211934790834ffa9bca8c01
Andrii Bratanin <zmmx-aerial@ukr.net>
2026-08-20 15:35:15 +0300

    ImplCSharp: make Docker_run scripts run the application against both registries

--- ImplCSharp/Docker_run.sh
+++ ImplCSharp/Docker_run.sh
@@ -3,6 +3,8 @@
 
 # Note: to debug container internals run:
 # docker run --rm -it -v ../Data:/Data --entrypoint /bin/sh opa-grepper
+
+# Executive proceedings
 docker run --rm \
     -e SOURCE_URL="https://data.gov.ua/dataset/22aef563-3e87-4ed9-92e8-d764dc02f426/resource/d1a38c08-0f3a-4687-866f-f28f50df7c46/download/28-ex_csv_asvp.zip" \
     -e ICONV="true" \
@@ -10,5 +12,12 @@ docker run --rm \
     -v ../Data:/Data \
     -v .:/Result \
     opa-grepper
-# Debtors:
-#    -e SOURCE_URL="https://data.gov.ua/dataset/783b9b50-faba-4cc9-a393-60485e395b1d/resource/e6ea76c1-01f4-4bd0-a282-7d92d6ecc2a1/download/29-ex_csv_erb.zip" \
+
+# Debtors
+docker run --rm \
+    -e SOURCE_URL="https://data.gov.ua/dataset/783b9b50-faba-4cc9-a393-60485e395b1d/resource/e6ea76c1-01f4-4bd0-a282-7d92d6ecc2a1/download/29-ex_csv_erb.zip" \
+    -e ICONV="true" \
+    -e RESULT_DIR="/Result" \
+    -v ../Data:/Data \
+    -v .:/Result \
+    opa-grepper
